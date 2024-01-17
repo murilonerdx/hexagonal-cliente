5b3f2ca6bd1a9dcbd92c3f0a1a473205f9b44006
println <murilo_pereira@terceiros.sicredi.com.br>
2024-01-17 19:04:52 -0300

    feat: criando user case para delete customer

---
+++ src/main/java/com/murilonerdx/hexagonal/application/core/usecase/DeleteCustomerUseCase.java
@@ -0,0 +1,20 @@
+package com.murilonerdx.hexagonal.application.core.usecase;
+
+import com.murilonerdx.hexagonal.application.core.domain.Customer;
+import com.murilonerdx.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.murilonerdx.hexagonal.application.ports.out.DeleteCustomerOutputPort;
+
+public class DeleteCustomerUseCase {
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+    private final DeleteCustomerOutputPort deleteCustomerOutputPort;
+
+    public DeleteCustomerUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort, DeleteCustomerOutputPort deleteCustomerOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerOutputPort = deleteCustomerOutputPort;
+    }
+
+    public void delete(String id){
+        Customer customer = findCustomerByIdInputPort.find(id);
+        deleteCustomerOutputPort.delete(customer.getId());
+    }
+}
